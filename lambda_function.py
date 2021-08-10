19d936dd6fefec010801514cab63a76b483049e5
tamagawa <tamagawa@arara.com>
2021-08-10 18:59:09 +0900

    lpp

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello test test7 from Lambda!')
+        'body': json.dumps('Hello test test8 from Lambda!')
     }
